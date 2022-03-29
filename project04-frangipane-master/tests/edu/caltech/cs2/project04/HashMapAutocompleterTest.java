d8b341317862ddc29cf164751a7dedba1ef0eb22
Allam Amzad <96650682+allamzad@users.noreply.github.com>
2022-03-28 20:12:50 -0700

    Add files via upload

---
+++ project04-frangipane-master/tests/edu/caltech/cs2/project04/HashMapAutocompleterTest.java
@@ -0,0 +1,61 @@
+package edu.caltech.cs2.project04;
+
+import edu.caltech.cs2.helpers.Inspection;
+import edu.caltech.cs2.interfaces.IDeque;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class HashMapAutocompleterTest {
+    private static String STRING_SOURCE = "src/edu/caltech/cs2/project04/HashMovieAutoCompleter.java";
+    static {
+        HashMovieAutoCompleter.populateTitles();
+    }
+    @Order(1)
+    @Tag("D")
+    @DisplayName("Tests complete(String val)")
+    @ParameterizedTest(name = "Test complete(\"{0}\")")
+    @CsvSource({
+            "mission: impossible, test_mission_impossible",
+            "the avengers, test_the_avengers",
+            "this, test_this",
+            "age of, test_age_of"
+    })
+    void complete(String lookup, String res_file) throws IOException {
+        IDeque<String> suggestions = HashMovieAutoCompleter.complete(lookup);
+        List<String> expected = new ArrayList<>();
+
+        Scanner fr = new Scanner(new File("data/" + res_file));
+
+        while (fr.hasNextLine()) {
+            String title = fr.nextLine();
+            expected.add(title);
+        }
+
+        MatcherAssert.assertThat(suggestions,
+                IsIterableContainingInAnyOrder.containsInAnyOrder(expected.toArray()));
+    }
+
+    @Order(0)
+    @Tag("D")
+    @DisplayName("Does not use or import disallowed classes")
+    @Test
+    public void testForInvalidClasses() {
+        // You may not import anything from java.util except for these classes
+        List<String> allowed = List.of("Arrays", "Map", "HashMap", "Set", "HashSet");
+        Inspection.assertNoImportsOfExcept(STRING_SOURCE,"java\\.util", allowed);
+        List<String> bannedUsages = List.of("java\\.lang\\.reflect", "java\\.io",
+                "java\\.util\\.(?!" + String.join("|", allowed) + ")");
+        Inspection.assertNoUsageOf(STRING_SOURCE, bannedUsages);
+    }
+}
